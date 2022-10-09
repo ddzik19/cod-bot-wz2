9ad8a81318de38a61c2a2fd0bd9bc50584fc2fd3
ddzik19 <58696796+ddzik19@users.noreply.github.com>
2022-10-09 10:16:46 +0100

    feat: working commands

--- commands/help.js
+++ commands/help.js
@@ -3,24 +3,20 @@
     Desc: Embed with all  commands
 */
 const {
-    MessageEmbed,
+    EmbedBuilder,
     GatewayIntentBits
 } = require('discord.js')
 
 module.exports = {
     name: "help",
     execute(msg) {
-        const newEmbed = new MessageEmbed()
+        const newEmbed = new EmbedBuilder()
             .setColor('a7fc00')
-            .setDescription("All existing commands")
             .setTitle("Commands")
             .addFields({
                 name: "!cod.help",
                 value: "Displays all commands"
             });
-
-        // creating the embed and sending it to the chat
-        console.log("im working")
         return msg.channel.send({
             embeds: [newEmbed]
         });
