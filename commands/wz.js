716e684c1914bc0216f121ee8aa1a5dc375335e3
ddzik19 <58696796+ddzik19@users.noreply.github.com>
2022-10-11 13:10:34 +0100

    feat: dynamic system for getting guns for each game. Throws error. Could be caused by the gunArray[] or passing in the parameters in bot.js line:59

--- commands/wz.js
+++ commands/wz.js
@@ -6,52 +6,54 @@ const { EmbedBuilder } = require("discord.js");
 const fs = require("fs");
 
 // getting all command files
-const guns = fs
-	.readdirSync("database/wz")
-	.filter((file) => file.endsWith(".png"));
-
-const gunArray = [];
-guns.map((gun) => {
-	var gun_object = gun.split(".")[0];
-	var gun_items = gun_object.split("_");
-	gunArray.push({
-		game: gun_items[0],
-		name: gun_items[1],
-		type: gun_items[2],
+const path = "database/wz";
+const dirs = fs.readdirSync(path).filter(function (file) {
+	return fs.statSync(`${path}/${file}`).isDirectory();
+});
+const getGunsArray = (game_name) => {
+	let gun_array = [];
+	let guns_items = fs
+		.readdirSync(`${path}/${game_name}`)
+		.filter((file) => file.endsWith(".png"));
+	guns_items.map((item) => {
+		var gun_object = item.split(".")[0];
+		var gun_items = gun_object.split("_");
+		gun_array.push({
+			name: gun_items[0],
+			type: gun_items[1],
+		});
 	});
+	return gun_array;
+};
+
+let gunArray = [];
+dirs.map((dir) => {
+	if (dir == "mw") {
+		gunArray = getGunsArray("mw");
+	}
+	if (dir == "cw") {
+		gunArray = getGunsArray("cw");
+	}
+	if (dir == "mw") {
+		gunArray = getGunsArray("vg");
+	}
 });
 
-const get_weapons = (game_name) => {
+const get_weapons = () => {
 	const fields = [];
 	gunArray.forEach((element) => {
-		if (game_name == "mw") {
-			fields.push({
-				inline: true,
-				value: element.type.toUpperCase(),
-				name: element.name.toUpperCase(),
-			});
-		}
-		if (game_name == "bo") {
-			fields.push({
-				inline: true,
-				value: element.type.toUpperCase(),
-				name: element.name.toUpperCase(),
-			});
-		}
-		if (game_name == "vg") {
-			fields.push({
-				inline: true,
-				value: element.type.toUpperCase(),
-				name: element.name.toUpperCase(),
-			});
-		}
+		fields.push({
+			inline: true,
+			value: element.type,
+			name: element.name,
+		});
 	});
 	return fields;
 };
 module.exports = {
 	name: "wz",
 	execute(msg, game_name) {
-		const fields = get_weapons(game_name);
+		const fields = get_weapons();
 		const newEmbed = new EmbedBuilder()
 			.setColor("00D4FF")
 			.setTitle(`Warzone: ${game_name.toUpperCase()}`)
