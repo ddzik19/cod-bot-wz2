557c70ce3f145467e642d06a42d3dbb2813a285c
ddzik19 <58696796+ddzik19@users.noreply.github.com>
2022-10-14 16:51:20 +0100

    feat: listing guns by type works

---
+++ commands/build.js
@@ -0,0 +1,56 @@
+const { EmbedBuilder, AttachmentBuilder } = require("discord.js");
+const fs = require("fs");
+
+const getGunImageUrl = (wz_option, game_name, gun_name) => {
+	let images = fs
+		.readdirSync(`database/${wz_option}/${game_name}/`)
+		.filter((file) => file.endsWith(".png"));
+
+	for (let i = 0; i <= images.length; i++) {
+		const element = images[i];
+		let image = element.split("_");
+		if (image[0] === gun_name) {
+			let imageInfo = [
+				`./database/${wz_option}/${game_name}/${element}`,
+				element,
+			];
+			return imageInfo;
+		}
+	}
+};
+
+const getColorForEmbed = (game_name) => {
+	let color = "";
+	if (game_name == "mw") {
+		color = "00D4FF";
+	} else if (game_name == "cw") {
+		color = "FF0000";
+	} else if (game_name == "vg") {
+		color = "FFFB00";
+	} else if (game_name == "aw") {
+		color = "2b2b2b";
+	} else if (game_name == "mw2") {
+		color = "FFFB00";
+	}
+	return color;
+};
+
+module.exports = {
+	name: "build",
+	description:
+		"Display a specific gun build for a game. \n EXAMPLE: !build.wz.cw.mp5",
+	execute(msg, wz_option, game_name, gun_name) {
+		const image = getGunImageUrl(wz_option, game_name, gun_name);
+		const attachment = new AttachmentBuilder(image[0]);
+		const color = getColorForEmbed(game_name);
+		const embed = new EmbedBuilder()
+			.setColor(color)
+			.setTitle(`Warzone: ${game_name.toUpperCase()}`)
+			.setDescription(gun_name)
+			.setImage((url = `attachment://${image[1]}`));
+		return msg.channel.send({
+			files: [attachment],
+			embeds: [embed],
+		});
+	},
+};
---
+++ commands/list.js
@@ -0,0 +1,72 @@
+/*
+    Author: Damian Dzik
+    Desc: Embed specific type of weapons for both warzone games
+*/
+const { EmbedBuilder } = require("discord.js");
+const fs = require("fs");
+
+const createEmbeds = (array, gun_type) => {
+	const pages = array.length / 25
+	let embeds = []
+	if(pages > 1){
+		for (let i = 0; i <= pages; i++) {
+			for (let j = 0; j < array.length; j + 25) {
+				let chunk = array.splice(j, j + 25);
+				let embed = new EmbedBuilder()
+					.setColor("Blurple")
+					.setTitle(`Warzone: ${gun_type.toUpperCase()} guns`)
+					.addFields(chunk);
+				embeds.push(embed);
+			}
+		}
+		return embeds
+	}else {
+		let embed = new EmbedBuilder()
+			.setColor("Blurple")
+			.setTitle(`Warzone: ${gun_type.toUpperCase()} guns`)
+			.addFields(array);
+		return embed;
+	}
+}
+
+module.exports = {
+	name: "list",
+	description:
+		"Display a specific gun build for a game. \n EXAMPLE: !build.wz.sr \n GUN TYPES: ar, smg, lmg, sr, mk ,ps, shg, tac",
+	execute(msg, wz_option, gun_type) {
+		const dir_result = fs.readdirSync(`database/${wz_option}`);
+		const games = dir_result.splice(1, dir_result.length);
+		let guns_to_display = [];
+		for (let game of games) {
+			let guns = fs
+				.readdirSync(`database/${wz_option}/${game}/`)
+				.filter((file) => file.endsWith(".png"));
+
+			for (let gun of guns) {
+				let g_name = gun.split(".")[0];
+				let g_type = g_name.split("_")[1];
+				if (gun_type === g_type) {
+					guns_to_display.push({
+						name: `${gun.split("_")[0]}`,
+						value: `${game}`,
+						inline: true
+					});
+				}
+			}
+		}
+
+		const embeds = createEmbeds(guns_to_display, gun_type)
+		if (embeds.length > 1) {
+			for (let i = 0; i < embeds.length; i++) {
+				msg.channel.send({
+					embeds: [embeds[i]],
+				});
+			}
+		} else {
+			return msg.channel.send({
+				embeds: [embeds],
+			});
+		}
+		return;
+	},
+};
